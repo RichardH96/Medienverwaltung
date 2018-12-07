346dd99cdeae8dff4f490a9aa81b51fa8ddcd46e
Humer Richard <humer.richard@gmx.at>
2018-12-07 23:44:46 +0100

    FormLogin added
    Form edited, started with code

---
+++ Wifi.Media.Login/FormLogin.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Wifi.Media.Login
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPasswordRepeat = new System.Windows.Forms.TextBox();
+            this.labelPasswordRepeat = new System.Windows.Forms.Label();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBoxUsername.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.Location = new System.Drawing.Point(235, 84);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(220, 22);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.Location = new System.Drawing.Point(60, 86);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(116, 18);
+            this.labelUsername.TabIndex = 1;
+            this.labelUsername.Text = "Benutzername:";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.Location = new System.Drawing.Point(78, 212);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(113, 35);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Einloggen";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBoxPassword.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(235, 124);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(220, 22);
+            this.textBoxPassword.TabIndex = 0;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(60, 126);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(77, 18);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "Passwort:";
+            // 
+            // textBoxPasswordRepeat
+            // 
+            this.textBoxPasswordRepeat.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.textBoxPasswordRepeat.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPasswordRepeat.Location = new System.Drawing.Point(235, 165);
+            this.textBoxPasswordRepeat.Name = "textBoxPasswordRepeat";
+            this.textBoxPasswordRepeat.Size = new System.Drawing.Size(220, 22);
+            this.textBoxPasswordRepeat.TabIndex = 0;
+            this.textBoxPasswordRepeat.Visible = false;
+            // 
+            // labelPasswordRepeat
+            // 
+            this.labelPasswordRepeat.AutoSize = true;
+            this.labelPasswordRepeat.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPasswordRepeat.Location = new System.Drawing.Point(60, 167);
+            this.labelPasswordRepeat.Name = "labelPasswordRepeat";
+            this.labelPasswordRepeat.Size = new System.Drawing.Size(169, 18);
+            this.labelPasswordRepeat.TabIndex = 1;
+            this.labelPasswordRepeat.Text = "Passwort wiederholen:";
+            this.labelPasswordRepeat.Visible = false;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.Font = new System.Drawing.Font("Arial", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogin.Location = new System.Drawing.Point(12, 9);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(504, 51);
+            this.labelLogin.TabIndex = 1;
+            this.labelLogin.Text = "LOGIN";
+            this.labelLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(327, 212);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(113, 35);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Abbrechen";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRegister.Location = new System.Drawing.Point(204, 212);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(113, 35);
+            this.buttonRegister.TabIndex = 3;
+            this.buttonRegister.Text = "Registrieren";
+            this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(528, 299);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPasswordRepeat);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxPasswordRepeat);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medienverwaltung";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPasswordRepeat;
+        private System.Windows.Forms.Label labelPasswordRepeat;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonRegister;
+    }
+}
+
---
+++ Wifi.Media.Login/FormLogin.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wifi.Media.Login
+{
+    public partial class FormLogin : Form
+    {
+        bool registermode = false;
+
+
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            registermode = true;
+            changeToRegister();
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (registermode)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            if (registermode)
+            {
+                registermode = false;
+                changeToRegister();
+            }
+            else
+            {
+
+            }
+        }
+
+
+
+        private void changeToRegister()
+        {
+            if (registermode)
+            {
+                this.labelPasswordRepeat.Visible = true;
+                this.textBoxPasswordRepeat.Visible = true;
+                this.labelLogin.Text = "REGISTRIEREN";
+
+                this.buttonRegister.Visible = false;
+
+                this.buttonLogin.Text = "Bestätigen";
+                this.buttonLogin.Top = 212;
+
+                this.buttonCancel.Top = 212;
+            }
+            else if (!registermode)
+            {
+                this.labelPasswordRepeat.Visible = false;
+                this.textBoxPasswordRepeat.Visible = false;
+                this.labelLogin.Text = "LOGIN";
+
+                this.buttonRegister.Visible = true;
+
+                this.buttonLogin.Text = "Einloggen";
+                this.buttonLogin.Top = 176;
+
+                this.buttonCancel.Top = 176;
+            }
+
+        }
+
+    }
+}
